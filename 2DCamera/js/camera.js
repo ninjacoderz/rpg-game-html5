cfa0c882a2c648b1029c40f1ebedecf29cc0456f
Binh Nguyen Thanh <binhnguyenthanh@Binhs-MacBook-Pro-2.local>
2026-04-10 00:32:52 +0700

    Camera

---
+++ 2DCamera/js/camera.js
@@ -0,0 +1,22 @@
+export class Camera {
+    constructor(map, width, height){
+        this.map = map;
+        this.width = width;
+        this.height = height;
+        this.x = 0;
+        this.y = 0;
+        this.maxX = map.cols * map.tileSize - this.width;
+        this.maxY = map.rows * map.tileSize - this.height;
+        this.speed = 256;
+    }
+
+    move(deltaTime, speedX, speedY ) {
+        this.x += speedX * this.speed * deltaTime;
+        this.y += speedY * this.speed * deltaTime;
+        this.x = Math.max(0, Math.min(this.x, this.maxX));
+        this.y = Math.max(0, Math.min(this.y, this.maxY));
+    }
+    render (ctx) {
+       
+    }
+}
---
+++ 2DCamera/js/main.js
@@ -0,0 +1,99 @@
+import { Camera } from "./camera.js";
+import { Map } from "./map.js";
+
+export const GAME_WIDTH = 640;
+export const GAME_HEIGHT = 640;
+
+class Game {
+    constructor() {
+        this.map = new Map();
+
+        this.camera = new Camera(this.map, GAME_WIDTH, GAME_HEIGHT);
+        this.keys = [];
+        window.addEventListener('keydown', e =>{
+            if(this.keys.indexOf(e.key) === -1) {
+                this.keys.unshift(e.key);
+            }
+        })
+
+        window.addEventListener('keyup', e => {
+            const index = this.keys.indexOf(e.key);
+
+            if (index > -1) {
+                this.keys.splice(index, 1);
+            }
+        });
+    }
+
+    update(deltaTime) {
+        let speedX = 0;
+        let speedY = 0;
+        if (this.keys[0] === 'ArrowLeft') speedX = -1;
+        if (this.keys[0] === 'ArrowRight') speedX = 1;
+        if (this.keys[0] === 'ArrowUp') speedY = -1;
+        if (this.keys[0] === 'ArrowDown') speedY = 1;
+
+        this.camera.move(deltaTime, speedX, speedY);
+
+    }
+    drawLayer(layer, ctx) {
+        const startCol = Math.floor(this.camera.x / this.map.tileSize );
+        const endCol = startCol + this.camera.width / this.map.tileSize;
+        const startRow = Math.floor(this.camera.y / this.map.tileSize)
+        const endRow = startRow + this.camera.height / this.map.tileSize;
+
+        const offsetX = -this.camera.x + startCol * this.map.tileSize;
+        const offsetY = -this.camera.y + startRow * this.map.tileSize;
+
+        for (let row = startRow; row < endRow; row++ ) {
+            for (let col = startCol; col < endCol; col++ ) {
+                const tile = this.map.getTile(layer, col, row);
+                const x = (col -startCol) * this.map.tileSize + offsetX;
+                const y = (row -startRow) * this.map.tileSize + offsetY;
+                ctx.drawImage(this.map.image, 
+                    (tile - 1) * this.map.image_tile % this.map.image.width,
+                    Math.floor((tile - 1) / this.map.image_cols) * this.map.image_tile,
+                    this.map.image_tile, 
+                    this.map.image_tile, 
+                    Math.round(x),
+                    Math.round(y),
+                    this.map.tileSize,
+                    this.map.tileSize
+                );
+                
+                // ctx.strokeRect(
+                //     col * this.map.tileSize,
+                //     row * this.map.tileSize,
+                //     this.map.tileSize,
+                //     this.map.tileSize
+                // )
+            }
+        }
+    }
+    render (ctx) {
+        this.drawLayer(0, ctx);
+        this.drawLayer(1, ctx);
+    }
+}
+
+window.addEventListener('load', function() {
+    const canvas = document.getElementById('gameCanvas');
+    const ctx = canvas.getContext('2d');
+    ctx.imageSmoothingEnabled = false;
+    canvas.width = GAME_WIDTH;
+    canvas.height = GAME_HEIGHT;
+
+
+    const game = new Game()
+    
+    let lastTime = 0;
+    function animate(timeStamp) {
+        const deltaTime = ( timeStamp - lastTime ) / 1000;
+
+        lastTime = timeStamp;
+        game.update(deltaTime);
+        game.render(ctx);
+        requestAnimationFrame(animate);
+    }
+    requestAnimationFrame(animate);
+})
